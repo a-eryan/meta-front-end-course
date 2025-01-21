439e5be5bf7958b2081d6e9ec36ee28d8b66f623
Anthony Eryan <72895267+a-eryan@users.noreply.github.com>
2025-01-20 21:46:13 -0500

    React Basics/Course 5 Mini Project

---
+++ Course5/src/App.js
@@ -0,0 +1,74 @@
+import { useState,useRef} from "react"; 
+import "./App.css";
+
+function App() { 
+  const inputRef = useRef(null); 
+  const resultRef = useRef(null); 
+  // When we write:
+  const [result, setResult] = useState(null);
+
+  // We get:
+  // result: the state variable (starts at 0)
+  // setResult: a function to update that variable
+  // Although we don't assign e, React creates and passes this event object whenever the button is clicked. (applies to all event handlers) 
+  function plus(e) { 
+    e.preventDefault(); //Stops <form> submission/page refresh
+    setResult((result) => result + Number(inputRef.current.value)); 
+  }; 
+ 
+  function minus(e) { 
+  	e.preventDefault(); //Stops <form> submission/page refresh
+    setResult((result) => result - Number(inputRef.current.value));  
+  };
+ 
+  function times(e) { 
+    e.preventDefault(); //Stops <form> submission/page refresh
+    setResult((result) => result * Number(inputRef.current.value));   
+  }; 
+ 
+  function divide(e) { 
+    e.preventDefault(); //Stops <form> submission/page refresh
+    setResult((result) => result / Number(inputRef.current.value));    
+  };
+ 
+  function resetInput(e) { 
+    // Add the code for the resetInput function 
+    e.preventDefault(); //Stops <form> submission/page refresh
+    (inputRef.current.value) = null;
+  }; 
+ 
+  function resetResult(e) {
+    e.preventDefault();
+    setResult(null);
+  }; 
+ 
+  return ( 
+    <div className="App"> 
+      <div>         {/* Add the subtract button */}
+        <h1>Simplest Working Calculator</h1> 
+      </div> 
+      <form> 
+        <p ref={resultRef}>Result: {result}</p> 
+        <input
+          pattern="[0-9]" 
+          ref={inputRef} 
+          type="number" 
+          placeholder="Type a number" 
+        /> 
+        <button onClick={plus}>Add</button> 
+        {/* Add the subtract button */}
+        <button onClick={minus}>Minus</button> 
+        {/* Add the multiply button */}
+        <button onClick={times}>Multiply</button> 
+        {/* Add the divide button */}
+        <button onClick={divide}>Divide</button> 
+        {/* Add the resetInput button */}
+        <button onClick={resetInput}>Reset Input</button>         
+        {/* Add the resetResult button */}
+        <button onClick={resetResult}>Reset Result</button>         
+      </form> 
+    </div> 
+  ); 
+} 
+ 
+export default App; 
